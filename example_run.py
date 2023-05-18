ffa8e2b956e21b41af3a9312b8710457edf773e4
ilmari99 <i.vahteristo@gmail.com>
2023-05-18 23:46:39 +0100

    Removed example

--- example_run.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-from MoskaEngine.Play.play_in_browser import parse_args, run_as_command_line_program
-
-if __name__ == '__main__':
-    args = parse_args()
-    print(args)
-    run_as_command_line_program(" ".join(sys.argv[1:]))
