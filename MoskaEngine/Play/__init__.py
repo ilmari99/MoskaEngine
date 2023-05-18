e2b88f73fe9425ced577783a60a5200bdfb20206
ilmari99 <i.vahteristo@gmail.com>
2023-05-18 23:22:01 +0100

    Added import of Game for relativity

--- MoskaEngine/Play/__init__.py
+++ MoskaEngine/Play/__init__.py
@@ -1,19 +1,16 @@
-# Check if MOSKA_ROOT_PATH environment variables exists.
-# If it does not, we run the top-level __init__.py file, which will create it.
 import os
 import sys
 def __initialize_pkg__():
-    # Add the parent folder to the path
-    root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    sys.path.append(root)
+    # Add the parent parent folder to the path
+    MRP = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    sys.path.append(MRP)
     # Define the path to the root folder of this library
-    os.environ["MOSKA_ROOT_PATH"] = root
-    #print("MOSKA_ROOT_PATH:", root)
+    MOSKA_ROOT_PATH = MRP
+    os.environ["MOSKA_ROOT_PATH"] = MOSKA_ROOT_PATH
 
-if "MOSKA_ROOT_PATH" in os.environ:
-    #print("MOSKA_ROOT_PATH environment variable found. Skipping top-level __init__.py file.")
-    #print("MOSKA_ROOT_PATH:", os.environ["MOSKA_ROOT_PATH"])
-    pass
-else:
-    #print("MOSKA_ROOT_PATH environment variable not found. Running top-level __init__.py file.")
+if "MOSKA_ROOT_PATH" not in os.environ:
     __initialize_pkg__()
+
+# Import packages from top level, so that modules in this package can use them
+from ..Player import *
+from ..Game import *
