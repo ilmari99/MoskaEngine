8e3ac40c7222273dce85b184fd6ddf9c5483ab47
ilmari99 <i.vahteristo@gmail.com>
2023-05-19 18:01:10 +0100

    Test if playing as json players works. This doesn't test human input, only automatically runs a game with 4 players

---
+++ Tests/test_Play_play_in_browser.py
@@ -0,0 +1,6 @@
+# This file is a very minimal example, and is provided for a friend
+import sys
+from MoskaEngine.Play.play_in_browser import play_as_human
+
+if __name__ == '__main__':
+    play_as_human(model_path="Model-nn1-BB", pred_format="bitmap",test=True)
