a3ef298654dd91489d357a89d784bf20decfdd60
ilmari99 <i.vahteristo@gmail.com>
2023-05-19 17:59:51 +0100

    Now plays 5 games instead of one

--- Tests/test_Play_benchmark.py
+++ Tests/test_Play_benchmark.py
@@ -9,19 +9,22 @@ from MoskaEngine.Player.NewRandomPlayer import NewRandomPlayer
 
 if __name__ == '__main__':
     pl_type = NewRandomPlayer
+    # Arguments for this player
     pl_kwargs = {
         "name" : "player1",
     }
+    # Arguments for the game
     game_kwargs = {
-        "log_level" : logging.INFO,
+        "log_level" : logging.DEBUG,
         "gather_data" : False,
-        "timeout" : 6,
+        "timeout" : 20,
     }
+    # Arguments shared by all players
     shared_player_kwargs = {
-        "log_level" : logging.INFO,
+        "log_level" : logging.DEBUG,
     }
     results = []
     clean_up()
     for benchmark in [BENCH1, BENCH2, BENCH3, BENCH4]:
-        results.append(run_benchmark(pl_type, pl_kwargs, benchmark, game_kwargs, shared_player_kwargs, cpus=1, chunksize=1,ngames=1))
+        results.append(run_benchmark(pl_type, pl_kwargs, benchmark, game_kwargs, shared_player_kwargs, cpus=5, chunksize=1,ngames=5))
     print(results)
