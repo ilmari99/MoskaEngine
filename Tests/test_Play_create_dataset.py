ef80456f8d6ee725a84e35f860ac1697e58c86b1
ilmari99 <i.vahteristo@gmail.com>
2023-05-19 18:00:13 +0100

    Now plays 10 and 8 games instead of 2

--- Tests/test_Play_create_dataset.py
+++ Tests/test_Play_create_dataset.py
@@ -6,15 +6,15 @@ from MoskaEngine.Play.create_dataset import create_dataset
 if __name__ == '__main__':
     gamekwargs = {
         "log_level" : logging.DEBUG,
-        "timeout" : 12,
+        "timeout" : 20,
         "gather_data":True,
     }
     shared_pl_kwargs = {
         "log_level" : logging.DEBUG,
     }
     
-    nsuc = create_dataset(2, 1, "test_dataset", 1, chunksize=1, use_HIF=False, verbose=True, players="random", gamekwargs=gamekwargs, shared_player_kwargs=shared_pl_kwargs)
-    assert nsuc == 2, f"Expected 2 succesful games, got {nsuc}."
+    nsuc = create_dataset(2, 5, "test_dataset", cpus=5, chunksize=1, use_HIF=False, verbose=True, players="random", gamekwargs=gamekwargs, shared_player_kwargs=shared_pl_kwargs)
+    assert nsuc == 10, f"Expected 10 succesful games, got {nsuc}."
     print()
-    nsuc = create_dataset(1, 2, "test_dataset", 1, chunksize=1, use_HIF=True, verbose=True, players="random", gamekwargs=gamekwargs, shared_player_kwargs=shared_pl_kwargs)
-    assert nsuc == 2, f"Expected 2 succesful games, got {nsuc}."
+    nsuc = create_dataset(4, 2, "test_dataset",cpus = 2, chunksize=1, use_HIF=True, verbose=True, players="random", gamekwargs=gamekwargs, shared_player_kwargs=shared_pl_kwargs)
+    assert nsuc == 8, f"Expected 8 succesful games, got {nsuc}."
