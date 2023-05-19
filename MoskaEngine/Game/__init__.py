14e2ba45142fbabd7f7273f6931a67ab7db57dd9
ilmari99 <i.vahteristo@gmail.com>
2023-05-19 17:47:33 +0100

    Removed path modification

--- MoskaEngine/Game/__init__.py
+++ MoskaEngine/Game/__init__.py
@@ -2,13 +2,10 @@ import os
 import sys
 def __initialize_pkg__():
     # Add the parent parent folder to the path
-    MRP = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    #sys.path.append(MRP)
     # Define the path to the root folder of this library
+    MRP = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     MOSKA_ROOT_PATH = MRP
     os.environ["MOSKA_ROOT_PATH"] = MOSKA_ROOT_PATH
 
 if "MOSKA_ROOT_PATH" not in os.environ:
     __initialize_pkg__()
-
-#from ..Player import *
