1006039c3b9640f2affab587f1815a28435750d4
ilmari99 <i.vahteristo@gmail.com>
2023-05-20 18:32:37 +0100

    Added readme to pypi and version 0.1.5

--- setup.py
+++ setup.py
@@ -6,15 +6,24 @@ requirements = [
     "tensorflow==2.11",
 ]
 
+# Read the README.md file
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='MoskaEngine',
-    version='0.1.4',
+    version='0.1.5',
     description='A card game engine for the card game Moska. Contains game engine, players, simulation tools, and human interface.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Ilmari Vahteristo',
     url="https://github.com/ilmari99/MoskaEngine",
     # Used libraries are in the requirements.txt file
     packages=find_packages(),
-    package_data={"MoskaEngine": ["Models/*/*.tflite"], "MoskaEngine": ["Play/PlayerConfigs/*.json"]},
+    # Include the model files, and the player config files
+    package_data={
+        'MoskaEngine': ['Models/*/*.tflite', 'Play/PlayerConfigs/*.json']
+    },
     install_requires=requirements,
     # Require python 3.6 or higher, but not 3.11 or higher
     python_requires='>=3.6, <3.11',
